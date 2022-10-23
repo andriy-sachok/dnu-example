90670ae0de95bc25250f3b8c1d37c87742f68208
Andriy Sachok <andriy.sachok@gmail.com>
2022-10-23 21:57:12 +0300

    connected news to db

--- src/components/news/NewsList.js
+++ src/components/news/NewsList.js
@@ -1,45 +1,32 @@
-import React from "react";
-import LoremIpsum from "react-lorem-ipsum";
+import React, { useState, useEffect } from "react";
 
 import NewsItem from "./NewsItem";
 
 import classes from "./NewsList.module.css";
 
-const DUMMY_NEWS = [
-  {
-    title: "Article 1",
-    text: <LoremIpsum />,
-  },
-  {
-    title: "Article 2",
-    text: <LoremIpsum />,
-  },
-  {
-    title: "Article 3",
-    text: <LoremIpsum />,
-  },
-  {
-    title: "Article 4",
-    text: <LoremIpsum />,
-  },
-  {
-    title: "Article 5",
-    text: <LoremIpsum />,
-  },
-  {
-    title: "Article 6",
-    text: <LoremIpsum />,
-  },
-];
-
 const NewsList = () => {
+  const [news, setNews] = useState([]);
+
+  const fetchNews = async () => {
+    const response = await fetch(
+      "https://dnu-news-default-rtdb.europe-west1.firebasedatabase.app/news.json"
+    );
+    const data = await response.json();
+
+    setNews(Object.entries(data));
+  };
+
+  useEffect(() => {
+    fetchNews();
+  }, []);
+
   return (
     <div className={classes.main}>
       <h1 className={classes.title}>News | All news</h1>
       <div>
         <div className={classes.newsContainer}>
-          {DUMMY_NEWS.map((el) => (
-            <NewsItem key={el.title} title={el.title} text={el.text} />
+          {news.map((el) => (
+            <NewsItem key={el[1].title} title={el[1].title} text={el[1].text} />
           ))}
         </div>
       </div>
